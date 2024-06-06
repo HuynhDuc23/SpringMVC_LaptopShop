aa493b03c05238be608f2bcf3dd3ebe9a549c335
ducdev <you@example.com>
2024-06-06 18:51:03 +0700

    init project with maven and spring mvc

---
+++ src/main/java/vn/com/demo/LapTopShopApplication.java
@@ -0,0 +1,13 @@
+package vn.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapTopShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapTopShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/demo/LapTopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapTopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
